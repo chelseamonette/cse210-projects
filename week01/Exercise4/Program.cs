47e5cd8817d99cf9d0c8c9cf7507d941375b2cf8
chelseamonette <mon24045@byui.edu>
2025-01-09 03:13:12 -0800

    Conpleted all the core requirements for this assignment by making a list of numbers from the user and finding the sum, average, and max from that list and stopping the list when the user typed "0".

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,32 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int newNumber;
+        float sumNumbers = 0;
+        float average;
+
+        do{
+            Console.Write("Enter number: ");
+            string enter = Console.ReadLine();
+            newNumber = int.Parse(enter);
+
+            numbers.Add(newNumber);
+            sumNumbers += newNumber;
+
+        } while (newNumber != 0);
+
+        Console.WriteLine($"The sum is: {sumNumbers}");
+
+        float amountNumbers = numbers.Count - 1;
+        average = sumNumbers / amountNumbers;
+        Console.WriteLine($"The average is: {average}");
+
+        int maxNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
