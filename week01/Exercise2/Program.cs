8b3fbf023b642542cc276094e1a308cb53000b4e
chelseamonette <mon24045@byui.edu>
2025-01-09 00:41:23 -0800

    I worked through the first two core requirements of the assignment and ran to make sure that it works.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What grade percentage do you have? ");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You earned an A");
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            Console.WriteLine("You earned a B");
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            Console.WriteLine("You earned a C");
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            Console.WriteLine("You earned a D");
+        }
+        else 
+        {
+            Console.WriteLine("You earned an F");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not earn a high enough grade to pass. Better luck next time.");
+        }
     }
 }
