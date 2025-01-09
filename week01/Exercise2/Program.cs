9504090dbae51ff01c588820f60329ee883b26b8
chelseamonette <mon24045@byui.edu>
2025-01-09 00:51:27 -0800

    Completed the third portion of the assignment and converted it all to just one print statement.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -7,27 +7,30 @@ class Program
         Console.Write("What grade percentage do you have? ");
         string gradePercentage = Console.ReadLine();
         int grade = int.Parse(gradePercentage);
+        string letterGrade = grade.ToString();
 
         if (grade >= 90)
         {
-            Console.WriteLine("You earned an A");
+            letterGrade = "A";
         }
         else if (grade >= 80 && grade < 90)
         {
-            Console.WriteLine("You earned a B");
+            letterGrade = "B";
         }
         else if (grade >= 70 && grade < 80)
         {
-            Console.WriteLine("You earned a C");
+            letterGrade = "C";
         }
         else if (grade >= 60 && grade < 70)
         {
-            Console.WriteLine("You earned a D");
+            letterGrade = "D";
         }
         else 
         {
-            Console.WriteLine("You earned an F");
+            letterGrade = "F";
         }
+
+        Console.WriteLine($"You earned a {letterGrade}");
 
         if (grade >= 70)
         {
