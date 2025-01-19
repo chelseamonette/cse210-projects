4970ddb231b5e4cc629a712d7f12cefa14bf1d42
chelseamonette <mon24045@byui.edu>
2025-01-18 22:23:34 -0800

    Began the process to Write the options for the beginning of the journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        while(true)
+        {
+            Console.WriteLine("1. Create New Entry");
+            Console.WriteLine("2. Display Journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4.Load the journal from a file");
+            Console.WriteLine("5.Quit");
+
+            Console.Write("Enter your selection: ");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine($"User Selected: {userInput}");
+
+        }
     }
 }
