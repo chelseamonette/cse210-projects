70561c70d2c24547fca2887ae4920590952bbdff
chelseamonette <mon24045@byui.edu>
2025-01-23 01:49:36 -0800

    worked through the activity.  Troubleshooted and compared against sample solution

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        string fractionString1 = fraction1.GetFractionString();
+        Console.WriteLine(fractionString1);
+        double decimalFraction1 = fraction1.GetDecimalValue();
+        Console.WriteLine(decimalFraction1);
+        
+        Fraction fraction2 = new Fraction(5);
+        string fractionString2 = fraction2.GetFractionString();
+        Console.WriteLine(fractionString2);
+        double decimalFraction2 = fraction2.GetDecimalValue();
+        Console.WriteLine(decimalFraction2);
+
+        Fraction fraction3 = new Fraction(3, 4);
+        string fractionString3 = fraction3.GetFractionString();
+        Console.WriteLine(fractionString3);
+        double decimalFraction3 = fraction3.GetDecimalValue();
+        Console.WriteLine(decimalFraction3);
+
+        Fraction fraction4 = new Fraction(1,3);
+        string fractionString4 = fraction4.GetFractionString();
+        Console.WriteLine(fractionString4);
+        double decimalFraction4 = fraction4.GetDecimalValue();
+        Console.WriteLine(decimalFraction4);
+
     }
 }
