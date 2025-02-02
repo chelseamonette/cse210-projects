17b2294cdc569c2e93e853609af6dab55498f7eb
chelseamonette <mon24045@byui.edu>
2025-02-02 04:38:47 -0800

    completed the bones of the Customer class from the diagram

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,20 @@
+/*1. The customer contains a name and an address.
+
+2. The name is a string, but the Address is a class.
+
+3. The customer should have a method that can return whether 
+they live in the USA or not. (Hint this should call a method on 
+the address to find this.)*/
+using System;
+
+public class Customer{
+private string _customerName;
+private Address _address;
+public Customer(string customerName, Address address){
+    _customerName = customerName;
+    _address = address;
+}
+public bool LivesInUSA(){
+    return true;
+}
+}
