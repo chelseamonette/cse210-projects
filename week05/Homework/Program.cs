45c191c7e96da3c1ceece1d6a09ba09f98ccc66b
chelseamonette <mon24045@byui.edu>
2025-02-06 16:30:43 -0800

    finished the learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Assignment assignment = new("Chelsea Monette", "Inheritance");
+        // string summary = assignment.GetSummary();
+        // Console.WriteLine(summary);
+        // MathAssignment mathAssignment = new("Chelsea Monette", "Inheritance", "7.3", "189-190");
+        //string summary = mathAssignment.GetSummary();
+        //string newAssigment = mathAssignment.GetHomeworkList();
+        //Console.WriteLine(summary);
+        //Console.WriteLine(newAssigment);
+        WritingAssignment writingAssignment = new("Chelsea Monette", "European History", "The Causes of World War II");
+        string summary = writingAssignment.GetSummary();
+        string title = writingAssignment.GetWritingInformation();
+        Console.WriteLine(summary);
+        Console.WriteLine(title);
+
+         
+
     }
 }
