f3a4e5e579e36c74da73c1a80f095ab38ff7f1e8
chelseamonette <mon24045@byui.edu>
2025-02-13 21:02:38 -0800

    code written for learning activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square1 = new("blue", 4);
+        square1.GetColor();
+        square1.GetArea();
+
+        Rectangle rectangle1 = new("red", 7, 10);
+        rectangle1.GetArea();
+
+        Circle circle = new("green", 15);
+        circle.GetArea();
+
+        List<Shape> shapes = new();
     }
 }
