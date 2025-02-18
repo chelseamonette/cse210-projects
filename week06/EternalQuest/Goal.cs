493fad6d19c62f92a57ca8220813d4a4559907a2
chelseamonette <mon24045@byui.edu>
2025-02-17 21:37:04 -0800

    finished the writing the code in the Goal class

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -10,8 +10,20 @@ public abstract class Goal{
     }
     public abstract void RecordEvent();
     public abstract bool IsComplete();
+    // This method should return the details of a goal that could 
+    // be shown in a list. It should include the checkbox, the short 
+    // name, and description. Then in the case of the ChecklistGoal 
+    // class, it should be overridden to shown the number of times 
+    // the goal has been accomplished so far.
     public string GetDetailsString(){
-        return "";
+        string checkbox = "[ ]";
+        if (IsComplete()){
+            checkbox = "[√]";
+        }
+        else{
+            return checkbox;
+        }
+        return $"1. {checkbox} {_shortName} ({_description})";
     }
     public abstract string GetStringRepresentation();
 }
